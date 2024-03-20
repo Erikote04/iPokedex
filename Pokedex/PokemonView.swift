be989bbaa6d24b63f22d2bf1cdc1c05e41bc4270
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-03-20 20:29:15 +0100

    Async images and @State

--- Pokedex/PokemonView.swift
+++ Pokedex/PokemonView.swift
@@ -1,20 +1,51 @@
 import SwiftUI
 
+let bulbasaurURL = URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png")
+
+let charmanderURL = URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png")
+
+let squitleURL = URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png")
+
+let pokemons = [
+    bulbasaurURL,
+    charmanderURL,
+    squitleURL,
+]
+
+let pokemonColor = [
+    Color.green,
+    Color.orange,
+    Color.cyan,
+]
+
 struct PokemonView: View {
+    
+    @State var index: Int = 0
+    
     var body: some View {
         VStack {
             Spacer()
             
+            AsyncImage(url: pokemons[index])
+            
             HStack {
                 NavigationButton(title: "<") {
-                    print("left")
+                    if index > 0 {
+                        index -= 1
+                    } else {
+                        index = pokemons.count - 1
+                    }
                 }
                 
                 NavigationButton(title: ">") {
-                    print("right")
+                    if index < pokemons.count - 1 {
+                        index += 1
+                    } else {
+                        index = 0
+                    }
                 }
             }
-        }
+        }.background(pokemonColor[index])
     }
 }
 
