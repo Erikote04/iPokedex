a604d61ae9814281c1c5c95d3186572a3a84dfa8
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-03-20 19:29:48 +0100

    Initial Commit

---
+++ Pokedex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pokedex
+//
+//  Created by Erik Sebastián de Erice Jerez on 20/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
