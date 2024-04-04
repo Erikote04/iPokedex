96cabaeb78b6cabca1649441d950a38b8cb20955
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-04-04 23:23:42 +0200

    URLSession and multi-threads

--- Pokedex/Model/Pokemon.swift
+++ Pokedex/Model/Pokemon.swift
@@ -1,7 +1,6 @@
 import SwiftUI
 
 struct Pokemon: Identifiable, Decodable {
-    
     struct Sprite: Decodable {
         struct Other: Decodable {
             struct Artwork: Decodable {
@@ -21,16 +20,34 @@ struct Pokemon: Identifiable, Decodable {
         let other: Other
     }
     
+    struct PokemonType: Decodable {
+        struct InnerPokemonType: Decodable {
+            let name: String
+            let url: String
+        }
+        
+        let type: InnerPokemonType
+    }
+    
     let id: Int
     let name: String
     let weight: Int
     let sprites: Sprite
+    let types: [PokemonType]
     
     var url: URL? {
-        nil
+        URL(string: sprites.other.officialArtwork.frontDefault)
     }
     
     var color: Color? {
-        Color.green
+        if types[0].type.name == "Earth" {
+            return Color.brown
+        } else if types[0].type.name == "Fire" {
+            return Color.red
+        } else if types[0].type.name == "Water" {
+            return Color.blue
+        }
+        
+        return Color.gray
     }
 }
--- Pokedex/api/PokeApi.swift
+++ Pokedex/api/PokeApi.swift
@@ -1,24 +1,48 @@
 import SwiftUI
 
-func getPokemons(completion: ([Pokemon]) -> Void) {
-    let bulbasaurURL = URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png")
-    let charmanderURL = URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png")
-    let squitleURL = URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png")
+func getPokemons(completion: @escaping ([Pokemon]) -> Void) {
+    var pokemons = [Pokemon]()
     
-    getPokemon("https://pokeapi.co/api/v2/pokemon/80", completion: { pokemon in
-            completion([ pokemon! ])
-    })
+    for id in 1..<10 {
+        getPokemon("https://pokeapi.co/api/v2/pokemon/\(id)", completion: { pokemon in
+            if let pokemon = pokemon {
+                pokemons.append(pokemon)
+                completion(pokemons)
+            }
+        })
+    }
 }
 
-func getPokemon(_ pokemonURL: String, completion: (Pokemon?) -> Void) {
-    let data = Data(pokemon1JSON.utf8)
+/// Returns a Pokemon from PokeAPI
+/// - Parameters:
+///    - pokemonURL: to pokemon
+///    - completion: this returns a Pokemon (if found and all is OK) in the Main Thread
+/// Example
+/// ```
+/// getPokemon("https://pokeapi.co/api/v2/pokemon/80", completion: { pokemon in
+///     completion([ pokemon! ])
+/// })
+/// ```
+func getPokemon(_ pokemonURL: String, completion: @escaping (Pokemon?) -> Void) {
+    guard let URL = URL(string: pokemonURL) else { return }
     
-    parsePokemonJSON(data) { pokemon in
-        completion(pokemon)
+    let request = URLRequest(url: URL)
+    let session = URLSession.shared
+    let task = session.dataTask(with: request) { data, response, error in
+        if error == nil,
+           let statusCode = (response as? HTTPURLResponse)?.statusCode,
+           let data = data {
+            parsePokemonJSON(data) { pokemon in
+                completion(pokemon)
+            }
+        }
     }
+    
+    task.resume()
 }
 
 func parsePokemonJSON(_ data: Data, completion: (Pokemon?) -> Void) {
     let pokemon = try? JSONDecoder().decode(Pokemon.self, from: data)
     completion(pokemon)
 }
+
