a5b19154539412aec9a5538745b55efdc4952b8e
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-04-06 01:07:15 +0200

    [Test]: Pagination Unit Testing

--- Pokedex/api/Page.swift
+++ Pokedex/api/Page.swift
@@ -1,19 +1,19 @@
 import Foundation
 
-struct Page {
-    let start: Int
-    let pageSize: Int
+public struct Page {
+    public let start: Int
+    public let pageSize: Int
     
-    init(start: Int = 1, pageSize: Int = 10) {
+    public init(start: Int = 1, pageSize: Int = 10) {
         self.start = start
         self.pageSize = pageSize
     }
     
-    func next() -> Page {
+    public func next() -> Page {
         Page(start: start + pageSize, pageSize: pageSize)
     }
     
-    func previous() -> Page {
-        Page(start: start + pageSize, pageSize: pageSize)
+    public func previous() -> Page {
+        start - pageSize < 0 ? self : Page(start: start + pageSize, pageSize: pageSize)
     }
 }
