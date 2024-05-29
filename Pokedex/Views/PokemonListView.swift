6a228439092ba08623287f95084700c859548b9c
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-05-29 12:50:00 +0200

    Search functionality in pokemon list

--- Pokedex/Views/PokemonListView.swift
+++ Pokedex/Views/PokemonListView.swift
@@ -2,17 +2,44 @@ import SwiftUI
 
 struct PokemonListView: View {
     let pokemons: [Pokemon]!
+    @State private var searchText = ""
+    
+    var filteredPokemons: [Pokemon] {
+        if searchText.isEmpty {
+            return pokemons
+        } else {
+            return pokemons.filter { $0.name.lowercased().contains(searchText.lowercased()) }
+        }
+    }
     
     var body: some View {
         NavigationStack {
-            List(pokemons) { pokemon in
+            List(filteredPokemons) { pokemon in
                 NavigationLink(destination: PokemonDetailsView(pokemon: pokemon)) {
                     PokemonListCellView(pokemon: pokemon)
+                }
+            }
+            .searchable(
+                text: $searchText,
+                prompt: "Search"
+            ) {
+                if !searchText.isEmpty {
+                    Text("TOP NAME MATCHES")
+                        .font(.footnote)
+                        .foregroundStyle(.secondary)
+                }
+                
+                ForEach(filteredPokemons) { pokemon in
+                    NavigationLink(destination: PokemonDetailsView(pokemon: pokemon)) {
+                        Text(pokemon.name.capitalized)
+                            .foregroundStyle(.black)
+                    }
                 }
             }
         }
     }
 }
+
 
 #Preview {
     PokemonListView(
