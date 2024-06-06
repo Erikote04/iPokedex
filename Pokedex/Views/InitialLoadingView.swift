a59829422a97f4b44a7aa9d9693d69bca2dbd587
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-06-06 17:59:57 +0200

    Pokeball image rotating in loading view

--- Pokedex/Views/InitialLoadingView.swift
+++ Pokedex/Views/InitialLoadingView.swift
@@ -1,14 +1,23 @@
 import SwiftUI
 
 struct InitialLoadingView: View {
+    @State private var isRotating = false
+    
     var body: some View {
         VStack {
-            ProgressView()
-                .progressViewStyle(CircularProgressViewStyle(tint: .white))
-                .padding()
+            Image("pokeball")
+                .rotationEffect(.degrees(isRotating ? 360 : 0))
+                .animation(
+                    Animation.linear(duration: 1)
+                        .repeatForever(autoreverses: false)
+                )
+                .onAppear {
+                    self.isRotating = true
+                }
             
             Text("Loading...")
                 .foregroundStyle(.white)
+                
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(.systemBlue))
